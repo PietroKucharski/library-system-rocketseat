8c20d8bad4d6cb93ba1830842f5bb8f567da6996
pietr <pietrokucharski@hotmail.com>
2025-06-21 00:19:45 -0300

    feat: Creating customer entity

---
+++ src/domain/entities/Customer.java
@@ -0,0 +1,66 @@
+package domain.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Customer {
+    private UUID id;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String email, LocalDate birthDate) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
